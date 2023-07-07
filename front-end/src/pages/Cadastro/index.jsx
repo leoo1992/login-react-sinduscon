a22a0e62c6cbf3863bcf8f9a901caced22a48a34
Leonardo Santos Custodio <santos-contato@hotmail.com.br>
2023-07-07 19:08:36 -0300

    tratamento cadastro

--- front-end/src/pages/Cadastro/index.jsx
+++ front-end/src/pages/Cadastro/index.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
 import api from "./axiosConfig";
+import ReactModal from "react-modal";
 
 import "./styles.css";
 
 const Cadastro = () => {
-  const navigate = useNavigate();
-
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    confirmPassword: "",
     nome: "",
     idade: "",
     profissao: "",
@@ -30,6 +29,39 @@ const Cadastro = () => {
     youtube: "",
   });
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [formErrors, setFormErrors] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    nome: "",
+    idade: "",
+    profissao: "",
+    descricao: "",
+    endereco: "",
+    bairro: "",
+    cidade: "",
+    estado: "",
+    telefone: "",
+    nome_empresa: "",
+    prestador_cliente: "",
+    cpf_cnpj: "",
+    linkedin: "",
+    instagram: "",
+    whatsapp: "",
+    telegram: "",
+    facebook: "",
+    youtube: "",
+  });
+
+  const errorRef = useRef(null);
+
+  useEffect(() => {
+    if (errorRef.current) {
+      errorRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [formErrors]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
@@ -38,21 +70,82 @@ const Cadastro = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await api.post("/cadastro", formData);
-      console.log(response.data);
+    // Verificar a correspondência de senha
+    if (formData.password !== formData.confirmPassword) {
+      setFormErrors({
+        ...formErrors,
+        confirmPassword: "As senhas não correspondem",
+      });
+      return;
+    } else {
+      setFormErrors({
+        ...formErrors,
+        confirmPassword: "",
+      });
+    }
 
-      navigate("/login");
+    const requiredFields = [
+      "email",
+      "password",
+      "confirmPassword",
+      "nome",
+      "idade",
+      "profissao",
+      "descricao",
+      "endereco",
+      "bairro",
+      "cidade",
+      "estado",
+      "telefone",
+      "nome_empresa",
+      "prestador_cliente",
+      "cpf_cnpj",
+      "linkedin",
+      "instagram",
+      "whatsapp",
+      "telegram",
+      "facebook",
+      "youtube",
+    ];
+
+    const errors = {};
+
+    requiredFields.forEach((field) => {
+      if (!formData[field]) {
+        errors[field] = "Campo obrigatório";
+      }
+    });
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+
+    const { confirmPassword, ...dataWithoutConfirmPassword } = formData;
+
+    try {
+      await api.post("/cadastro", dataWithoutConfirmPassword);
+      setModalOpen(true);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleModalClose = () => {
+    try {
+      setModalOpen(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const isFormValid = Object.keys(formErrors).length === 0;
+
   return (
-    <>
+    <div className="container">
       <h1 className="text-center bg-cadastro m-0 p-0 text-white">Cadastro</h1>
       <div className="bg-cadastro d-flex justify-content-center align-items-center p-5 fw-bold">
-        <div className="cadastro w-25 border-success rounded-3 p-4">
+        <div className="cadastro w-100 w-md-50 w-lg-25 border-success rounded-3 p-4">
           <form onSubmit={handleSubmit}>
             <div className="form-group pt-4">
               <label>Email:</label>
@@ -64,9 +157,12 @@ const Cadastro = () => {
                 onChange={handleChange}
                 required
               />
+              {formErrors.email && (
+                <div className="error-message">{formErrors.email}</div>
+              )}
             </div>
             <div className="form-group pt-4">
-              <label>Password:</label>
+              <label>Senha:</label>
               <input
                 type="password"
                 name="password"
@@ -75,6 +171,25 @@ const Cadastro = () => {
                 onChange={handleChange}
                 required
               />
+              {formErrors.password && (
+                <div className="error-message">{formErrors.password}</div>
+              )}
+            </div>
+            <div className="form-group pt-4">
+              <label>Confirmar Senha:</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                className="form-control"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+              {formErrors.confirmPassword && (
+                <div className="error-message" ref={errorRef}>
+                  {formErrors.confirmPassword}
+                </div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Nome:</label>
@@ -85,6 +200,9 @@ const Cadastro = () => {
                 value={formData.nome}
                 onChange={handleChange}
               />
+              {formErrors.nome && (
+                <div className="error-message">{formErrors.nome}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Idade:</label>
@@ -95,6 +213,9 @@ const Cadastro = () => {
                 value={formData.idade}
                 onChange={handleChange}
               />
+              {formErrors.idade && (
+                <div className="error-message">{formErrors.idade}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Profissão:</label>
@@ -105,6 +226,9 @@ const Cadastro = () => {
                 value={formData.profissao}
                 onChange={handleChange}
               />
+              {formErrors.profissao && (
+                <div className="error-message">{formErrors.profissao}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Descrição:</label>
@@ -115,6 +239,9 @@ const Cadastro = () => {
                 value={formData.descricao}
                 onChange={handleChange}
               />
+              {formErrors.descricao && (
+                <div className="error-message">{formErrors.descricao}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Endereço:</label>
@@ -125,6 +252,9 @@ const Cadastro = () => {
                 value={formData.endereco}
                 onChange={handleChange}
               />
+              {formErrors.endereco && (
+                <div className="error-message">{formErrors.endereco}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Bairro:</label>
@@ -135,6 +265,9 @@ const Cadastro = () => {
                 value={formData.bairro}
                 onChange={handleChange}
               />
+              {formErrors.bairro && (
+                <div className="error-message">{formErrors.bairro}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Cidade:</label>
@@ -145,6 +278,9 @@ const Cadastro = () => {
                 value={formData.cidade}
                 onChange={handleChange}
               />
+              {formErrors.cidade && (
+                <div className="error-message">{formErrors.cidade}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Estado:</label>
@@ -155,6 +291,9 @@ const Cadastro = () => {
                 value={formData.estado}
                 onChange={handleChange}
               />
+              {formErrors.estado && (
+                <div className="error-message">{formErrors.estado}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Telefone:</label>
@@ -165,6 +304,9 @@ const Cadastro = () => {
                 value={formData.telefone}
                 onChange={handleChange}
               />
+              {formErrors.telefone && (
+                <div className="error-message">{formErrors.telefone}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Nome da Empresa:</label>
@@ -175,6 +317,9 @@ const Cadastro = () => {
                 value={formData.nome_empresa}
                 onChange={handleChange}
               />
+              {formErrors.nome_empresa && (
+                <div className="error-message">{formErrors.nome_empresa}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Prestador/Cliente:</label>
@@ -185,6 +330,11 @@ const Cadastro = () => {
                 value={formData.prestador_cliente}
                 onChange={handleChange}
               />
+              {formErrors.prestador_cliente && (
+                <div className="error-message">
+                  {formErrors.prestador_cliente}
+                </div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>CPF/CNPJ:</label>
@@ -195,6 +345,9 @@ const Cadastro = () => {
                 value={formData.cpf_cnpj}
                 onChange={handleChange}
               />
+              {formErrors.cpf_cnpj && (
+                <div className="error-message">{formErrors.cpf_cnpj}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>LinkedIn:</label>
@@ -205,6 +358,9 @@ const Cadastro = () => {
                 value={formData.linkedin}
                 onChange={handleChange}
               />
+              {formErrors.linkedin && (
+                <div className="error-message">{formErrors.linkedin}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Instagram:</label>
@@ -215,6 +371,9 @@ const Cadastro = () => {
                 value={formData.instagram}
                 onChange={handleChange}
               />
+              {formErrors.instagram && (
+                <div className="error-message">{formErrors.instagram}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>WhatsApp:</label>
@@ -225,6 +384,9 @@ const Cadastro = () => {
                 value={formData.whatsapp}
                 onChange={handleChange}
               />
+              {formErrors.whatsapp && (
+                <div className="error-message">{formErrors.whatsapp}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Telegram:</label>
@@ -235,6 +397,9 @@ const Cadastro = () => {
                 value={formData.telegram}
                 onChange={handleChange}
               />
+              {formErrors.telegram && (
+                <div className="error-message">{formErrors.telegram}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>Facebook:</label>
@@ -245,6 +410,9 @@ const Cadastro = () => {
                 value={formData.facebook}
                 onChange={handleChange}
               />
+              {formErrors.facebook && (
+                <div className="error-message">{formErrors.facebook}</div>
+              )}
             </div>
             <div className="form-group pt-4">
               <label>YouTube:</label>
@@ -255,16 +423,32 @@ const Cadastro = () => {
                 value={formData.youtube}
                 onChange={handleChange}
               />
+              {formErrors.youtube && (
+                <div className="error-message">{formErrors.youtube}</div>
+              )}
             </div>
             <div className="text-center pt-5">
-            <button type="submit" className="btn btn-outline-success fw-bold">
+              <button
+                type="submit"
+                className="btn btn-outline-success fw-bold"
+                disabled={!isFormValid}
+              >
                 Cadastrar
               </button>
             </div>
           </form>
         </div>
       </div>
-    </>
+      <ReactModal
+        isOpen={modalOpen}
+        onRequestClose={handleModalClose}
+        contentLabel="Mensagem de Cadastro"
+      >
+        <h2>Cadastro realizado com sucesso!</h2>
+        <p>Clique sair para retornar ao menu de login.</p>
+        <button onClick={handleModalClose}>Sair</button>
+      </ReactModal>
+    </div>
   );
 };
 
